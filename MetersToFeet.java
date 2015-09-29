cb249a1c919af345bc3fd5c8c6a7b91c6bf370fd
Seth Pinckney <sethpinckney@Seths-MacBook-Pro.local>
2015-09-29 15:45:23 -0400

    Here is the commit of the source code

---
+++ MetersToFeet.java
@@ -0,0 +1,18 @@
+public class MetersToFeet {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("");
+		
+		for(int m = 1; m <= 100; m++) {
+			
+			float f = m * 3.28084f;
+			
+			System.out.println(m+" meters	"+f+" feet	  ");
+			
+			{
+										
+			}
+		}
+	}
+}
